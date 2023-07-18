73e8c4aa23f95b8a97e27cb02a1078617e614e8f
Mert-Gunes <91959456+Mert-Gunes@users.noreply.github.com>
2023-07-18 12:24:01 +0300

    Create food.dart

---
+++ food.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FoodsPage extends StatelessWidget {
+  const FoodsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      color: Colors.purple,
+    );
+  }
+}
