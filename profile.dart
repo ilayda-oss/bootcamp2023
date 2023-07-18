70a155bab8c95b35b40bff34dc5420d58d1dac9a
Mert-Gunes <91959456+Mert-Gunes@users.noreply.github.com>
2023-07-18 12:22:39 +0300

    Create profile.dart

---
+++ profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromARGB(255, 168, 222, 170),
+      child: Icon(Icons.circle, size: 185),
+    );
+  }
+}
