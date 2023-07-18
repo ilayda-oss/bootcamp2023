c32dba3ac7f31731823ea8f0c2583c41e04b1f7a
Mert-Gunes <91959456+Mert-Gunes@users.noreply.github.com>
2023-07-18 12:14:53 +0300

    Delete lib directory

--- lib/core/constants/consts.dart
+++
@@ -1,4 +0,0 @@
-export './firebase_consts.dart';
-
-export 'package:velocity_x/velocity_x.dart';
-export 'package:flutter/material.dart';
--- lib/core/init/theme/app_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-@immutable
-class AppTheme {
-  const AppTheme(this.context);
-  final BuildContext context;
-
-  ThemeData get theme => ThemeData.light().copyWith();
-}
