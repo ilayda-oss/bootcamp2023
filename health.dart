243ff0448519a094626c0ca91bb32b18f125c746
Mert-Gunes <91959456+Mert-Gunes@users.noreply.github.com>
2023-07-18 12:23:09 +0300

    Create health.dart

---
+++ health.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HealthPage extends StatelessWidget {
+  const HealthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
